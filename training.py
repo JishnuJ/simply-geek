131117abcf17820754981bfcb7c5479d6cda341b
JishnuJ <jishnujraj97@gmail.com>
2018-08-26 07:08:51 +0530

    Update training.py

--- training.py
+++ training.py
@@ -36,7 +36,6 @@ def getImagesAndLabels(path):
         id = int(os.path.split(imagePath)[-1].split(".")[1])
 
         faces = detector.detectMultiScale(img_numpy)
-
         for (x,y,w,h) in faces:
 
             faceSamples.append(img_numpy[y:y+h,x:x+w])
